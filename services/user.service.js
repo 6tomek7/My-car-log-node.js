19fcb121b531179d8c5725965c12a8a5ce3900be
Tom <krzysiekwichura@gmail.com>
2024-10-28 21:57:05 +0100

    add additional properties for user details

--- services/user.service.js
+++ services/user.service.js
@@ -3,15 +3,16 @@ const pool = require("../config/db");
 
 class UserService {
   static async getUser(userId) {
-    const user = await pool.execute("SELECT * FROM users WHERE id = ?", [
-      userId,
-    ]);
+    const user = await pool.execute("SELECT * FROM users WHERE id = ?", [userId]);
     const userData = user[0][0];
     return new User(
       userData.id,
       userData.username,
       userData.email,
-      userData.password
+      userData.language,
+      userData.mode,
+      userData.birthday,
+      userData.gender
     );
   }
 }
